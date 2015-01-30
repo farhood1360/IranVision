f3a7285d6944beccfbfeefd5241598d6a70a30f0
Farhood Rashidi <farhud.rashidi@gmail.com>
2015-01-29 21:23:09 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -25,6 +25,8 @@ class Database {
     private $delete;
     private $message;
 
+    // *************************************************************    CONSTRUCT FUNCTIONS   ******************************************************
+
     //connection() function
     //Craete the database configuration
     public function connection(){
@@ -36,7 +38,9 @@ class Database {
     public function select(){
         mysql_select_db(self::DB_NAME) or die(mysql_error());
     }
-       
+    
+    // *************************************************************    INSERT FUNCTIONS   ******************************************************
+
     //insert_content() function
     //Insert the new records to content table.
     //@param $author
@@ -48,6 +52,22 @@ class Database {
             $this->table = "Content";
             $this->insert = "INSERT INTO $this->table(author_name, web_page, titr, content) VALUES('{$author}', '{$page}', '{$title}', '{$content}')";
 	        $this->sql = mysql_query($this->insert);
+        }catch(Exception $e){
+            echo "There is a problem on insertion to database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
+    //insert_comment() function
+    //Insert the new records to comment table.
+    //@param $author
+    //@param $page
+    //@param $comment
+    public function insert_comment($author, $page, $comment){
+        try{
+            $this->table = "Comment";
+            $this->insert = "INSERT INTO $this->table(author_name, web_page, comment) VALUES('{$author}', '{$page}', '{$comment}')";
+            $this->sql = mysql_query($this->insert);
         }catch(Exception $e){
             echo "There is a problem on insertion to database! Please try again. " . $e->getMessage();
             exit();
@@ -113,6 +133,117 @@ class Database {
         }
     }
 
+    // *************************************************************    UPDATE FUNCTIONS   ******************************************************
+
+    //update_content_like() function
+    //Update the records of content table.  
+    //@param $contentid
+    //@param $contentlike
+    public function update_content_like($contentid, $contentlike){
+        try{
+            $this->table = "Content";
+            $this->update = "UPDATE $this->table SET likes = '{$contentlike}' WHERE content_id= '{$contentid}'";
+            $this->sql = mysql_query($this->update);
+            if($this->sql){
+                $this->message = "The number of likes is ".$contentlike.".<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem for update this database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
+
+    //update_content_dislike() function
+    //Update the records of content table.  
+    //@param $contentid
+    //@param $contentdislike
+    public function update_content_dislike($contentid, $contentdislike){
+        try{
+            $this->table = "Content";
+            $this->update = "UPDATE $this->table SET dislikes = '{$contentdislike}' WHERE content_id= '{$contentid}'";
+            $this->sql = mysql_query($this->update);
+            if($this->sql){
+                $this->message = "The number of dislikes is ".$contentdislike.".<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem for update this database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
+    //update_comment_like() function
+    //Update the records of comment table.  
+    //@param $commentid
+    //@param $commentlike
+    public function update_comment_like($commentid, $commentlike){
+        try{
+            $this->table = "Comment";
+            $this->update = "UPDATE $this->table SET likes = '{$commentlike}' WHERE comment_id= '{$commentid}'";
+            $this->sql = mysql_query($this->update);
+            if($this->sql){
+                $this->message = "The number of likes is ".$commentlike. ".<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem for update this database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
+    //update_comment_dislike() function
+    //Update the records of comment table.  
+    //@param $commentid
+    //@param $commentdislike
+    public function update_comment_dislike($commentid, $commentdislike){
+        try{
+            $this->table = "Comment";
+            $this->update = "UPDATE $this->table SET dislikes = '{$commentdislike}' WHERE comment_id= '{$commentid}'";
+            $this->sql = mysql_query($this->update);
+            if($this->sql){
+                $this->message = "The number of dislikes is ".$commentdislike.".<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem for update this database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
+    //update_comment_number_content() function
+    //Update the records of content table.  
+    //@param $contenttId
+    //@param $ccontentNum
+    public function update_comment_number_content($contenttId, $ccontentNum){
+        try{
+            $this->table = "Content";
+            $this->update = "UPDATE $this->table SET num_comments = '{$ccontentNum}' WHERE content_id= '{$contenttId}'";
+            $this->sql = mysql_query($this->update);
+            if($this->sql){
+                $this->message = "The number of comments is ".$ccontentNum. ".<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem for update this database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
+    //update_comment_number_comment() function
+    //Update the records of comment table.  
+    //@param $commentId
+    //@param $ccommentNum
+    public function update_comment_number_comment($commentId, $ccommentNum){
+        try{
+            $this->table = "Comment";
+            $this->update = "UPDATE $this->table SET num_comments = '{$ccommentNum}' WHERE comment_id= '{$commentId}'";
+            $this->sql = mysql_query($this->update);
+            if($this->sql){
+                $this->message = "The number of comments is ".$ccommentNum. ".<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem for update this database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
     //update_member() function
     //Update the records of members table.  
     //@param $lname
@@ -131,24 +262,6 @@ class Database {
             }
         }catch(Exception $e){
             echo "There is a problem for update this database! Please try again. " . $e->getMessage();
-            exit();
-        }
-    }
-
-    //delete_member() function
-    //Delete the records of members table.  
-    //@param $lname
-    //@param $fname
-    public function delete_member($lname, $fname){
-        try{
-            $this->table = "Members";
-            $this->delete = "DELETE FROM $this->table WHERE last_name= '{$lname}' AND first_name= '{$fname}'";
-            $this->sql = mysql_query($this->delete);
-            if($this->sql){
-                $this->message = "The information of ".$fname." ".$lname." is deleted.<br/>";
-            }
-        }catch(Exception $e){
-            echo "There is a problem on delete the records from database! Please try again. " . $e->getMessage();
             exit();
         }
     }
@@ -177,6 +290,27 @@ class Database {
         }
     }
 
+    // *************************************************************    DELETE FUNCTIONS   ******************************************************
+
+
+    //delete_member() function
+    //Delete the records of members table.  
+    //@param $lname
+    //@param $fname
+    public function delete_member($lname, $fname){
+        try{
+            $this->table = "Members";
+            $this->delete = "DELETE FROM $this->table WHERE last_name= '{$lname}' AND first_name= '{$fname}'";
+            $this->sql = mysql_query($this->delete);
+            if($this->sql){
+                $this->message = "The information of ".$fname." ".$lname." is deleted.<br/>";
+            }
+        }catch(Exception $e){
+            echo "There is a problem on delete the records from database! Please try again. " . $e->getMessage();
+            exit();
+        }
+    }
+
     //delete_editor() function
     //Delete the records of editors table.  
     //@param $lname
@@ -195,11 +329,13 @@ class Database {
             exit();
         }
     }
+
+    // *************************************************************    QUERY FUNCTIONS   ******************************************************
     
-    //query() function
+    //query_content() function
     //Select the filtered records from content table.
     //@param $Webpage
-    public function query($Webpage){
+    public function query_content($Webpage){
         $this->table = "Content";
         $this->select = "SELECT * FROM $this->table WHERE web_page = '$Webpage'";
         $this->row = mysql_query("SELECT web_page FROM $this->table WHERE web_page = '$Webpage'");
@@ -208,7 +344,32 @@ class Database {
         if($this->result === FALSE){die("Error querying databa.");}
         echo "<h2>" .$this->page['web_page']. "</h2><br>";
         while(($this->record = mysql_fetch_row($this->result)) !== FALSE){
-            echo "<h3>".$this->record[2]."</h3><article><span>By:".$this->record[0]."</span><p>".$this->record[3]."<span><a href='like.php'>Like | <a href='like.php'> Comment</a></span></p></article><br/>";
+            echo "<h3>".$this->record[3]."</h3><article><span>By:".$this->record[1]."</span><p>".$this->record[4].
+            "<span><a href='../model/like.php?content_id=".$this->record[0]."&content_like=".$this->record[6]."&page_id=".$this->record[2].
+            "'><img src='../image/like.png' width ='20' height ='20' alt='like' title='Like' id='like'/></a> ".$this->record[6].
+            " | <a href='../model/dislike.php?content_id=".$this->record[0]."&content_dislike=".$this->record[7]."&page_id=".$this->record[2].
+            "'><img src='../image/dislike.png' width ='20' height ='20' alt='dislike' title='Dislike' id='dislike'/></a> ".$this->record[7].
+            " | <a href='../controller/admin/comment_num.php?content_id=".$this->record[0]."&num_comments_content=".$this->record[5]."&page_id=".$this->record[2].
+            "' class='toggle'><img src='../image/comment.png' width ='20' height ='20' alt='comment' title='Comment' id='comment'/></a>".$this->record[5]."</span><div class='box'></div></article>";
+        }
+    }
+
+    //query_comment() function
+    //Select the filtered records from comment table.
+    //@param $Webpage
+    public function query_comment($Webpage){
+        $this->table = "Comment";
+        $this->select = "SELECT * FROM $this->table WHERE web_page = '$Webpage'";
+        $this->result = mysql_query($this->select);
+        if($this->result === FALSE){die("Error querying databa.");}
+        while(($this->record = mysql_fetch_row($this->result)) !== FALSE){
+            echo "<hr/><span>By:".$this->record[1]."</span><p class='comment'>&diams;".$this->record[3].
+            "<span><a href='../model/like.php?comment_id=".$this->record[0]."&comment_like=".$this->record[5]."&page_id=".$this->record[2].
+            "'><img src='../image/like.png' width ='20' height ='20' alt='like' title='Like' id='like'/></a> ".$this->record[5].
+             " | <a href='../model/dislike.php?comment_id=".$this->record[0]."&comment_dislike=".$this->record[6]."&page_id=".$this->record[2].
+            "'><img src='../image/dislike.png' width ='20' height ='20' alt='dislike' title='Dislike' id='dislike'/></a> ".$this->record[6].
+            " | <a href='../controller/admin/comment_num.php?comment_id=".$this->record[0]."&num_comments_comment=".$this->record[4]."&page_id=".$this->record[2].
+            "' class='toggle'><img src='../image/comment.png' width ='20' height ='20' alt='comment' title='Comment' id='comment'/></a>".$this->record[4]."</span></p><div class='box'></div>";
         }
     }
 
@@ -259,6 +420,8 @@ class Database {
             echo "<tr><td>".$this->record[0]."</td><td>".$this->record[1]."</td><td>".$this->record[2]."</td><td>".$this->record[3]."</td><td>".$this->record[4]."</td></tr>";
         }
     }
+
+    // *************************************************************    DESTRUCT FUNCTIONS   ******************************************************
 
     //showMessage() function
     //@return $message
